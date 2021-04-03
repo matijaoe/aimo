bec66383fe88dc8fea5781a71a300e721f466e0c
Matija Osrecki <matija.osrecki@gmail.com>
2021-04-03 14:54:52 +0200

    inital commit :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:prettier/recommended',
+		'plugin:nuxt/recommended',
+	],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		],
+		'no-unused-vars': 'warn',
+	},
+};
