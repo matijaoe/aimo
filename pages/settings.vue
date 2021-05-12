48adfb16b88904624e55c9aef5d09de9564b6801
Patrik Slovic <patrikslovic@gmail.com>
2021-05-12 05:22:20 +0200

    settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,3 +1,103 @@
 <template>
-	<h1 class="text-5xl">Settings page</h1>
+	<div>
+		<h1 class="text-5xl">Settings</h1>
+		<section class="mt-20">
+			<div id="name" class="flex space-x-12">
+				<vs-input
+					v-model.trim="currentUser.fname"
+					label-placeholder="First Name"
+					autocomplete="off"
+				/>
+				<vs-input
+					v-model.trim="currentUser.lname"
+					label-placeholder="Last Name"
+					autocomplete="off"
+				/>
+				<vs-input
+					v-model.trim="birthdayChosen"
+					type="date"
+					label-placeholder="Birthday"
+					autocomplete="off"
+				/>
+			</div>
+
+			<div class="flex space-x-12 mt-12">
+				<vs-input
+					v-model.trim="currentUser.occupation"
+					label-placeholder="Occupation"
+					autocomplete="off"
+				/>
+				<vs-input
+					v-model.trim="currentUser.bio"
+					label-placeholder="Profile Bio"
+					autocomplete="off"
+				/>
+			</div>
+			<div class="space-x-7 mt-12">
+				<vs-select
+					v-model="countryChosen"
+					filter
+					placeholder="Country"
+					autocomplete="off"
+				>
+					<vs-option
+						v-for="(countryOption, index) in getCountries"
+						:key="countryOption.alpha3Code"
+						:label="countryOption.name"
+						:value="index + 1"
+					>
+						<div class="flex items-center gap-2">
+							<BaseAvatar size="xs" :src="countryOption.flag" />
+							<p class="text-sm font-medium">
+								{{ countryOption.name }}
+							</p>
+						</div>
+					</vs-option>
+				</vs-select>
+			</div>
+			<div class="mt-16">
+				<BaseButton type="button" mode="warn"
+					>Confirm changes
+				</BaseButton>
+			</div>
+		</section>
+	</div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import BaseAvatar from 'UI/BaseAvatar';
+import BaseButton from 'UI/BaseButton';
+
+export default {
+	components: { BaseAvatar, BaseButton },
+	data() {
+		return {
+			countryIndex: '',
+		};
+	},
+	computed: {
+		...mapGetters(['currentUser', 'getCountries']),
+		country() {
+			if (this.countryIndex !== '') {
+				console.log(this.currentUser.birthday);
+				return this.getCountries[this.countryIndex - 1];
+			}
+			return '';
+		},
+		birthday() {
+			return this.currentUser.birthday.toDate();
+		},
+		countryChosen() {
+			const countryIndex = this.getCountries.findIndex(
+				(country) => country.alpha3Code === this.currentUser.countryCode
+			);
+			return countryIndex + 1;
+		},
+		birthdayChosen() {
+			console.log(this.currentUser.birthday.toDate());
+			return this.currentUser.birthday.toDate();
+		},
+	},
+};
+</script>
